19f1b7834b3b6b27a30fcc4b28b269a1768a8dcd
TechNing39 <leeyujineee@gmail.com>
2025-04-15 20:23:35 +0900

    [feat] add FloatingActionButton to Home Screen #1

--- app/src/main/java/com/example/rewear/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/rewear/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.rewear.databinding.FragmentHomeBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
 
@@ -22,15 +23,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val fab: FloatingActionButton = binding.fabAdd
+        fab.setOnClickListener{
+
         }
         return root
     }
