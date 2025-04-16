743ce3f6df3496ca860a46497bf24a641bf6b9f7
TechNing39 <leeyujineee@gmail.com>
2025-04-16 21:50:34 +0900

    [feat] implement HomeScreen Banner UI #1

--- app/src/main/java/com/example/rewear/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/rewear/ui/home/HomeFragment.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.rewear.databinding.FragmentHomeBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,8 +23,31 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val swapCount = 1
+        val donationCount = 5
+
+        val messageView: TextView = binding.userMessage
+        val digitLayout: LinearLayout = binding.digitLayout
+
+        if (swapCount == 0 && donationCount == 0) {
+            messageView.visibility = View.VISIBLE
+            digitLayout.visibility = View.GONE
+        } else {
+            messageView.visibility = View.GONE
+            digitLayout.visibility = View.VISIBLE
+
+            val swapDigits = String.format("%02d", swapCount)
+            val donationDigits = String.format("%02d", donationCount)
+
+            binding.swapDigit1.text = swapDigits[0].toString()
+            binding.swapDigit2.text = swapDigits[1].toString()
+            binding.donationDigit1.text = donationDigits[0].toString()
+            binding.donationDigit2.text = donationDigits[1].toString()
+
+        }
+
         val fab: FloatingActionButton = binding.fabAdd
-        fab.setOnClickListener{
+        fab.setOnClickListener {
 
         }
         return root
