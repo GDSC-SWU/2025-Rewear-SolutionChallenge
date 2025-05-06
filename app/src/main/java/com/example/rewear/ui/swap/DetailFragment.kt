6ad4be7f5497ac032bf653629793faee81b6c63a
TechNing39 <leeyujineee@gmail.com>
2025-05-06 20:57:04 +0900

    [feat] Add location section to detail page #11

--- app/src/main/java/com/example/rewear/ui/swap/DetailFragment.kt
+++ app/src/main/java/com/example/rewear/ui/swap/DetailFragment.kt
@@ -1,15 +1,23 @@
 package com.example.rewear.ui.swap
 
 import android.app.Dialog
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.rewear.R
 import com.example.rewear.databinding.DialogEnlargedImageBinding
 import com.example.rewear.databinding.FragmentDetailBinding
 import com.example.rewear.ui.home.Clothes
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 class DetailFragment : Fragment() {
@@ -34,32 +42,57 @@ class DetailFragment : Fragment() {
         }
 
         val clothes = arguments?.getParcelable<Clothes>("selectedClothes")
-        clothes?.let {selectedClothes->
-            binding.titleText.text = selectedClothes.name
-            binding.locationText.text=selectedClothes.location
-            binding.clothesLabel.text=selectedClothes.label
-            binding.descriptionText.text=selectedClothes.description
-            binding.swapMethod.text=selectedClothes.swapMethod
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            googleMap.clear()
 
-            val imageAdapter = DetailImageAdapter(selectedClothes.imageList) {imageUrl ->
+            clothes?.let { selectedClothes ->
+                val location = selectedClothes.location
+                val geocoder = Geocoder(requireContext())
+                val addressList = geocoder.getFromLocationName(location, 1)
+
+                if (addressList != null && addressList.isNotEmpty()) {
+                    val latLng = LatLng(addressList[0].latitude, addressList[0].longitude)
+
+                    val customMarker=BitmapDescriptorFactory.fromResource(R.drawable.ic_marker)
+
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                    googleMap.addMarker(
+                        MarkerOptions()
+                            .position(latLng)
+                            .title(selectedClothes.name)
+                            .icon(customMarker)
+                    )
+                }
+            }
+        }
+        clothes?.let { selectedClothes ->
+            binding.titleText.text = selectedClothes.name
+            binding.locationText.text = selectedClothes.location
+            binding.clothesLabel.text = selectedClothes.label
+            binding.descriptionText.text = selectedClothes.description
+            binding.swapMethod.text = selectedClothes.swapMethod
+            binding.location.text = selectedClothes.location
+
+            val imageAdapter = DetailImageAdapter(selectedClothes.imageList) { imageUrl ->
                 showEnlargedImage(imageUrl)
             }
             binding.detailImg.adapter = imageAdapter
             binding.dotsIndicator.setViewPager2(binding.detailImg)
 
-            binding.icZoom.setOnClickListener{
-                val currentPosition=binding.detailImg.currentItem
-                if(selectedClothes.imageList.isNotEmpty()){
-                    val currentImageUrl=selectedClothes.imageList[currentPosition]
+            binding.icZoom.setOnClickListener {
+                val currentPosition = binding.detailImg.currentItem
+                if (selectedClothes.imageList.isNotEmpty()) {
+                    val currentImageUrl = selectedClothes.imageList[currentPosition]
                     showEnlargedImage(currentImageUrl)
                 }
             }
         }
     }
 
-    private fun showEnlargedImage(imageUrl:Int){
-        val dialog= Dialog(requireContext())
-        val binding=DialogEnlargedImageBinding.inflate(layoutInflater)
+    private fun showEnlargedImage(imageUrl: Int) {
+        val dialog = Dialog(requireContext())
+        val binding = DialogEnlargedImageBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
 
         binding.enlargedImageView.setImageResource(imageUrl)
